1e5451d1ad2a6fa25df95c69a3740a59c54ecf89
is682431 <39606491+is682431@users.noreply.github.com>
2018-06-13 17:46:55 -0500

    changed structure after understanding c code

--- ex1practica.asm
+++ ex1practica.asm
@@ -6,20 +6,10 @@ r: .word 0, 0, 0
 
 .text
 
-	add $a0, $zero, $zero	# initalize i
-	add $a1, $zero, $zero	# initalize j
-	addi $t0, $zero, 3	# used to compare if j < 3
- 	la $s0, v		
- 	la $s1, m
- 	la $s2, r
- i:
- 	beq $a0, 3, exit 	# while i != 3 do i 
- 	 
- 	 
- 	addi $a0, $zero, 1   	# i++
- 
-j: 		
- 	beq $a1, 3, exit	# while j != 3 do j 
- 	
- 	addi $a1, $zero, 1	# j++
-exit:	
+main:
+
+la $a0, v	#load v	
+la $a1, r	#load r
+la $a2, m	#load m
+jal matrix
+j exit
