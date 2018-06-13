a937855ad59b2a90e4b05f4fc3d62021e47ede3e
is682431 <39606491+is682431@users.noreply.github.com>
2018-06-13 17:14:20 -0500

    added for i and j

--- ex1practica.asm
+++ ex1practica.asm
@@ -10,4 +10,14 @@ r: .word 0, 0, 0
 	add $a1, $zero, $zero	# initalize j
 	addi $t0, $zero, 3	# used to compare if j < 3
  
+ i:
+ 	beq $a0, 3, exit 	# while i != 3 do i 
+ 	
+ 	 
+ 	addi $a0, $zero, 1   	# i++
  
+j: 		
+ 	beq $a1, 3, exit	# while j != 3 do j 
+ 	
+ 	addi $a1, $zero, 1	# j++
+exit:	
